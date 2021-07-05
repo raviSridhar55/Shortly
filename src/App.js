5d6b5a75fde37b3147413198c101daba0fa10638
raviSridhar55 <61363065+raviSridhar55@users.noreply.github.com>
2021-07-05 17:42:30 +0530

    Navbar and React Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+import Navbar from './Components/Layout/Navbar';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+padding: 0;
+margin:0;
+box-sizing:border-box;
+  }
+   body {
+    background-color: #eee;
+    font-family: 'poppins', sans-serif; 
+    color : #9e9aa7;
+    font-size: 18px;
+    color: #333;
+    font-weight: 700;
+   }
+`;
+
+const Container = styled.div`
+  max-width: 1440px;
+  margin: auto;
+  padding: 0 10px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <GlobalStyle />
+      <Container>
+        <Navbar />
+      </Container>
     </div>
   );
 }
